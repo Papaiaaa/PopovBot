e01243125d642ef3ed2949a8ecb4e9f9df34e5f2
Papaiaaa <51392518+Papaiaaa@users.noreply.github.com>
2023-02-21 14:32:24 +0300

    111укеукьdsgwe

--- scr/main.py
+++ scr/main.py
@@ -1,8 +1,12 @@
+import time
+
 import telebot
 from telebot import types
 
 
 from datetime import datetime
+
+from telebot.types import KeyboardButton
 
 import config
 TOKEN =config.TOKEN1
@@ -11,7 +15,8 @@ import pyperclip
 
 import about
 about_message = about.about_txt
-
+import exp
+exp_message = exp.exp_txt
 import edu
 tusur = edu.edu1_txt
 tsu = edu.edu2_txt
@@ -35,6 +40,7 @@ def start(message):
     btn3 = types.KeyboardButton('Обо мне')
     btn4 = types.KeyboardButton('Контакты')
     markup.add(btn1,btn2,btn3,btn4)
+    #markup = types.ReplyKeyboardMarkup(resize_keyboard=True).add(KeyboardButton('Отправить свой контакт ☎️', request_contact=True)).add(KeyboardButton('Резюме', request_contact=True)).add(KeyboardButton('Резюме', request_contact=True)).add(KeyboardButton('Резюме', request_contact=True))
     greetings = f'Привет, <b>{message.from_user.first_name} {message.from_user.last_name}</b>\nПожалуйста, воспользутесь навигацией ниже:'
     bot.send_message(message.chat.id, greetings, parse_mode='html', reply_markup=markup)
 
@@ -44,7 +50,8 @@ def education(message):
     btn1 = types.KeyboardButton('ТУСУР `11')
     btn2 = types.KeyboardButton('ТГУ `15')
     btn3 = types.KeyboardButton('Нетология `22')
-    markup.add(btn1, btn2, btn3)
+    btn4 = types.KeyboardButton('Главное Меню')
+    markup.add(btn1,btn2,btn3,btn4)
     final_message = "Выбери один из вариантов ниже:"
     bot.send_message(message.chat.id, final_message, reply_markup=markup)
 @bot.message_handler(commands=['about'])
@@ -54,10 +61,15 @@ def about(message):
 
 @bot.message_handler(commands=['contacts'])
 def contacts(message):
-    bot.send_message(message.chat.id, contact_message, parse_mode='html')
-
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
+    btn1 = types.KeyboardButton('Обратная свзяь')
+    markup.add(btn1)
+    bot.send_message(message.chat.id, contact_message, reply_markup=markup,  parse_mode='html')
 @bot.message_handler(commands=['callme'])
 def callme(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
+    btn1 = types.KeyboardButton('Обратная свзяь')
+    markup.add(btn1)
     usder = message.from_user.username
     print(usder)
     print(current_datetime)
@@ -65,7 +77,7 @@ def callme(message):
     my_file.write('@' + message.from_user.username + ' ' + current_datetime + '\n')
     my_file.close()
     call_message = "В близжайшее время с Вами свяжутся"
-    bot.send_message(message.chat.id, call_message, parse_mode='html')
+    bot.send_message(message.chat.id, call_message, reply_markup=markup, parse_mode='html')
 @bot.message_handler(content_types=['text'])
 def mess(message):
     get_message_bot = message.text.strip().lower()
@@ -73,7 +85,8 @@ def mess(message):
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
         btn1 = types.KeyboardButton('Вывести на экран')
         btn2 = types.KeyboardButton('Скачать CV')
-        markup.add(btn1, btn2)
+        btn3 = types.KeyboardButton('Главное Меню')
+        markup.add(btn1, btn2, btn3)
         final_message = "Выбери один из вариантов ниже:"
         bot.send_message(message.chat.id, final_message, reply_markup=markup)
 
@@ -82,20 +95,16 @@ def mess(message):
         btn1 = types.KeyboardButton('ТУСУР `11')
         btn2 = types.KeyboardButton('ТГУ `15')
         btn3 = types.KeyboardButton('Нетология `22')
-        markup.add(btn1, btn2, btn3)
+        btn4 = types.KeyboardButton('Главное Меню')
+        markup.add(btn1, btn2, btn3, btn4)
         final_message = "Выбери один из вариантов ниже:"
         bot.send_message(message.chat.id, final_message, reply_markup=markup)
 
-        #bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAIBBmPstpQHEeNaYTHHLqFrmY8Qq9TyAAJ2JgACS_FpS54nHFUr5pb2LgQ')
-        #bot.send_message(message.chat.id, tusur, parse_mode='html')
-        #bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAIBCGPstpbYl_UtFaG0o_Zkyb-WCRrAAAIeJgAChORpSz8_zuUFfdC6LgQ')
-        #bot.send_message(message.chat.id, tsu, parse_mode='html')
-        #bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAIBCmPstpi9WSMamGXvm4H4dZ39EgOWAAJhJgACJ01hS15tjpWCfVRELgQ')
-        #bot.send_message(message.chat.id, netology, parse_mode='html')
     elif get_message_bot == "обо мне":
         bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAIBBGPstpI2hketg-noGW2wYZZs9fxfAALPIgACbKNpS3eQe7GdYF-_LgQ')
         bot.send_message(message.chat.id, about_message, parse_mode='html')
-
+    elif get_message_bot == "вывести на экран":
+        bot.send_message(message.chat.id, exp_message, parse_mode='html')
     elif get_message_bot == "тусур `11":
         bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAIBBmPstpQHEeNaYTHHLqFrmY8Qq9TyAAJ2JgACS_FpS54nHFUr5pb2LgQ')
         bot.send_message(message.chat.id, tusur, parse_mode='html')
@@ -106,5 +115,29 @@ def mess(message):
         bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAICN2Ps_Hk-5KQSleM2IZPnHn0jt2i_AAI_JAAC0KlpS55w2_0wCx3dLgQ')
         bot.send_message(message.chat.id, netology, parse_mode='html')
     elif get_message_bot == "контакты":
-        bot.send_message(message.chat.id, contact_message, parse_mode='html')
-bot.polling(none_stop=True)
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
+        btn1 = types.KeyboardButton('Обратная свзяь')
+        markup.add(btn1)
+        bot.send_message(message.chat.id, contact_message, reply_markup=markup, parse_mode='html')
+    elif get_message_bot == "скачать cv":
+        markup = types.InlineKeyboardMarkup()
+        markup.add(types.InlineKeyboardButton("Скачать", url="https://ru.wikipedia.org/wiki/%D0%9A%D0%B0%D0%B7%D0%B0%D1%85%D1%81%D1%82%D0%B0%D0%BD"))
+        bot.send_message(message.chat.id, 'Резюме', parse_mode='html', reply_markup=markup)
+    elif get_message_bot == "главное меню":
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
+        btn1 = types.KeyboardButton('Резюме')
+        btn2 = types.KeyboardButton('Образование')
+        btn3 = types.KeyboardButton('Обо мне')
+        btn4 = types.KeyboardButton('Контакты')
+        markup.add(btn1, btn2, btn3, btn4)
+        greetings = f'Пожалуйста, воспользутесь навигацией ниже:'
+        bot.send_message(message.chat.id, greetings, parse_mode='html', reply_markup=markup)
+
+#bot.polling(none_stop=True)
+
+while True:
+    try:
+        bot.polling(none_stop=True)
+    except Exception as e:
+        print(e)
+        time.sleep(15)
